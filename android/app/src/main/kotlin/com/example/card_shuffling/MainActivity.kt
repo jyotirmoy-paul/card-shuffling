27c861ef440fe1ae1163ba69b2687cc871fd4167
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-01-20 20:10:41 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/card_shuffling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_shuffling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:card_shuffling/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Card Shuffling',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
